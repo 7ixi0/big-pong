c191eb969777bd7c99588f1d326487d77339dac1
Dario Crosa <darcros7@gmail.com>
2019-11-23 18:37:35 +0100

    aggiunta coda prima di entrare in partita

---
+++ server/src/gameQueue.js
@@ -0,0 +1,81 @@
+const { EventEmitter } = require('events');
+
+module.exports = class GameQueue extends EventEmitter {
+  constructor(io) {
+    super();
+    this.q = [];
+    this.io = io;
+
+    this.gameRunning = false;
+  }
+
+  addToQueue(id) {
+    const socket = this.io.sockets.sockets[id];
+    if (!socket) return;
+    if (socket.disconnected) return;
+
+    this.q.push(id);
+    this.emit('newPlayer');
+
+    this.matchmaking();
+  }
+
+  matchmaking() {
+    const players = this.getPlayers(2);
+    if (!!players) {
+      if (this.gameRunning) return;
+
+      this.gameRunning = true;
+      this.emit('gameReady', players);
+    }
+  }
+
+  abortGame() {
+    this.gameRunning = false;
+    this.emit('gameEnded');
+
+    this.matchmaking();
+  }
+
+  removeIds(ids = []) {
+    this.q = this.q.filter(id => ids.indexOf(id) === -1);
+  }
+
+  removeId(id) {
+    this.removeIds([id]);
+  }
+
+  getPlayers(n) {
+    const players = [];
+    const invalidIds = [];
+
+    for (let i = 0; i < this.q.length; i++) {
+      const id = this.q[i];
+
+      const socket = this.io.sockets.sockets[id];
+      if (!socket || (socket && socket.disconnected)) {
+        invalidIds.push(id);
+        continue;
+      }
+      players.push(socket);
+
+      if (players.length === n) break;
+    }
+
+    this.removeIds(invalidIds);
+    if (players.length === n) {
+      this.removeIds(players.map(p => p.id));
+      return players;
+    } else {
+      return null;
+    }
+  }
+
+  get lenght() {
+    return this.q.length;
+  }
+
+  shift(n) {
+    return this.q.shift(n);
+  }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,44 +1,77 @@
 const io = require('socket.io')();
+const GameQueue = require('./gameQueue');
 
-let gameRunning = false;
+const gameQueue = new GameQueue(io);
+gameQueue.on('newPlayer', () => console.log(`${gameQueue.lenght} giocatori connessi`));
 
 io.on('connect', socket => {
   console.log('Socket connesso!');
 
-  // Role è uno di: display, waiting, player
-  // display: il display del gioco
-  // waiting: controller in attesa di giocare
-  // player: controller in partitata 
-  let role = '';
+  const registerDisplay = () => {
+    console.log('Display registrato!');
+    socket.join('display');
+  };
 
-  socket.on('join', data => {
-    console.log('join', data);
-    switch (data.role) {
+  const registerPlayer = () => {
+    console.log('Giocatore registato!');
+    socket.join('waiting');
+    gameQueue.addToQueue(socket.id);
+    socket.on('disconnect', () => gameQueue.removeId(socket.id));
+  };
+
+  socket.on('join', ({ role }) => {
+    console.log('> join', role);
+    switch (role) {
       case 'display':
-        console.log('Display registrato!');
-        role = 'display';
+        registerDisplay();
         break;
-
       case 'controller':
-        console.log('Giocatore connesso!');
-        role = gameRunning ? 'wating' : 'player';
+        registerPlayer();
         break;
     }
-
-    socket.join(role);
   });
+});
 
-  // pacchetto per spostare una paletta
-  // formato: { side: 'left', position: 0.35 }
-  // side: è 'left' o 'right' e si riferisce alla paletta da spostare
-  // position è la posizione a cui spostare il paletto, normalizzata (da 0 a 1)
-  socket.on('movePaddle', (data) => {
-    console.log(role, data);
-    if (role !== 'player') return;
+gameQueue.on('gameReady', ([player1, player2]) => {
+  console.log('Inizio gioco!');
 
-    console.log(data);
-    io.to('display').volatile.emit('movePaddle', data);
-  });
+  const join = (player) => {
+    player.leave('waiting');
+    player.join('playing');
+  };
+
+  const leave = player => player.disconnect(true);
+
+  /*
+  pacchetto per spostare una paletta
+  formato: { side: 'left', position: 0.35 }
+  side: è 'left' o 'right' e si riferisce alla paletta da spostare
+  position è la posizione a cui spostare il paletto, normalizzata (da 0 a 1)
+  */
+
+  // inoltra i pacchetti dai controller al display
+  const fowardPackets = socket =>
+    socket.on('movePaddle', (data) => {
+      io.to('display').volatile.emit('movePaddle', data);
+    });
+
+  const abort = () => {
+    leave(player1);
+    leave(player2);
+    gameQueue.abortGame();
+  };
+
+  const setup = player => {
+    join(player);
+    fowardPackets(player);
+    player.on('disconnect', () => {
+      console.log('Un giocatore ha abbandonato. Termino partita.');
+      abort();
+    });
+  }
+
+  setup(player1);
+  setup(player2);
 });
 
 io.listen(3000);
