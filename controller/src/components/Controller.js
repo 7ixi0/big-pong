023700fa130040fb0461f5fb6afec372d909001e
Dario Crosa <darcros7@gmail.com>
2019-11-25 17:38:47 +0100

    riscitto controller

--- controller/src/components/Controller.js
+++ controller/src/components/Controller.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Draggable from 'react-draggable';
 import { map } from '../util';
 import { socket } from '../socket';
 import { useInterval } from '../hooks/useInterval';
 import '../styles/controller.css';
 
 export const Controller = ({ side }) => {
-  const [value, setValue] = React.useState(50);
+  const controllerDiv = React.useRef();
+  const [bounds, setBounds] = React.useState();
+  const [range, setRange] = React.useState();
 
+  React.useEffect(() => {
+    const { top, bottom } = controllerDiv.current.getBoundingClientRect();
+
+    const range = bottom - top - 150; // 150 è l'altezza della paletta
+    setRange(bottom - top - 150);
+
+    // I bounds del <Draggable> sono relativi a quanto può muoversi in ogni direzione,
+    // quindi dobbiamo considerare che parte dall'angolo in alto a sinistra
+    setBounds({
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: range,
+    });
+  }, [controllerDiv]);
+
+  // Questa roba con le variabili va contro tutte le good practices di React,
+  // però è il modo più performante che ho trovato quindi andrà bene così
+  let value = 0;
   let lastValue = 0;
   useInterval(() => {
     if (value === lastValue) return;
     lastValue = value;
 
-    const position = map(value, 0, 100, 1, 0);
+    const position = map(value, 0, range, 0, 1);
     socket.emit('movePaddle', { position });
   }, 5);
 
@@ -22,19 +44,16 @@ export const Controller = ({ side }) => {
       <p className={`controller-side ${side}`}>
         LATO {side === 'left' ? 'SINISTRO' : 'DESTRO'}
       </p>
-      <div className={`controller ${side}`}>
-        <input
-          className="controller-input"
-          type="range"
-          min={0}
-          max={100}
-          step={0.1}
-          orient="vertical"
-
-          value={value}
-          onChange={e => setValue(e.target.value)}
-        />
-        <p></p>
+      <div ref={controllerDiv} className={`controller ${side}`}>
+        <Draggable
+          axis="y"
+          bounds={bounds}
+          onDrag={(_event, position) => {
+            value = position.y;
+          }}
+        >
+          <div className="controller-paddle" />
+        </Draggable>
       </div>
     </React.Fragment>
   );
